8508b7e21f0f7abb17f870a572c3c60a1fe65738
dmsgid8456 <hyangol77@gmail.com>
2019-09-09 18:53:12 +0900

    Set Contributor

--- src/test/java/list/CoArrayListTest.java
+++ src/test/java/list/CoArrayListTest.java
@@ -1,5 +1,21 @@
 package list;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CoArrayListTest {
 
+    @Test
+    public void testInit(){
+       List<String> arraylist=new CoArrayList();
+       List<String> testlist=new ArrayList<String>();
+
+
+       assertThat(arraylist).isInstanceOf(List.class);
+        assertThat(arraylist).isInstanceOf(CoArrayList.class);
+    }
 }
