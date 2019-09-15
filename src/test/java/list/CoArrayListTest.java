e19edeab1ac1b551e2a5b7d804fb3cc6e195c329
dmsgid8456 <hyangol77@gmail.com>
2019-09-15 16:26:03 +0900

    Added test source code #1
     * 시간 관계상 test code를 다 작성하지는 못했습니다.
     * Test Code 목록
       - init()
       - add()/addAll()
       - remove()/removeAll()
       - get()/set()
       - clear()
       - contains()/containsAll()
       - retain()
       - indexOf()/lastindexOf()
       - isEmpty()
       - sublist()

--- src/test/java/list/CoArrayListTest.java
+++ src/test/java/list/CoArrayListTest.java
@@ -3,7 +3,9 @@ package list;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,7 +14,6 @@ public class CoArrayListTest {
     @Test
     public void testInit() {
         List<String> arraylist = new CoArrayList();
-
 
         assertThat(arraylist).isInstanceOf(List.class);
         assertThat(arraylist).isInstanceOf(CoArrayList.class);
@@ -36,7 +37,109 @@ public class CoArrayListTest {
        assertThat(testlist.size()).isEqualTo(5);
        arraylist.addAll(testlist);
        assertThat(arraylist.size()).isEqualTo(10);
-
-
     }
+    @Test
+    public void testRemove(){
+        List<String> arraylist=new CoArrayList();
+        int arraySize=arraylist.size()-1;
+        arraylist.remove(arraySize);
+        assertThat(arraylist.size()).isEqualTo(arraySize-1);
+    }
+    @Test
+    public void testRemoveAll(){
+        List<String> arraylist=new CoArrayList();
+        List<String> testlist=new ArrayList();
+        arraylist.removeAll(testlist);
+       assertThat(arraylist.size()).isEqualTo(0);
+    }
+    @Test
+    public void testGet(){
+        List<String> arraylist= new CoArrayList();
+        for(int i=0;i<5;i++){
+            arraylist.add(""+i);
+        }
+        assertThat(arraylist.get(4)).isEqualTo(4);
+    }
+    @Test
+    public void testSet(){
+        List<String> arraylist=new CoArrayList();
+        arraylist.set(0,"abcde");
+        assertThat(arraylist.get(0)).isEqualTo("abcde");
+    }
+    @Test
+    public void testClear(){
+        List<String> arraylist=new CoArrayList();
+        arraylist.clear();
+        assertThat(arraylist.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void testContains(){
+        List<String> arraylist=new CoArrayList();
+        arraylist.add("a");
+        arraylist.add("b");
+        arraylist.add("c");
+        arraylist.contains("a");
+        assertThat(arraylist.size()).isEqualTo(2);
+    }
+    @Test
+    public void testContainsAll(){
+        List<String> arraylist=new CoArrayList();
+        List<String> testlist=new ArrayList();
+        for(int i=0;i<5;i++){
+            arraylist.add(""+i);
+            testlist.add(""+(i+1));
+        }
+        arraylist.containsAll(testlist);
+        assertThat(arraylist.size()).isEqualTo(1);
+    }
+    @Test
+    public void testRetainAll(){
+        List<String> arraylist=new CoArrayList();
+        List<String> testlist=new ArrayList();
+        for(int i=0;i<5;i++){
+            arraylist.add(""+i);
+            testlist.add(""+(i+1));
+        }
+        arraylist.retainAll(testlist);
+        assertThat(arraylist.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void testIndexOf(){
+        List<String> arraylist=new CoArrayList();
+        arraylist.add("a");
+        arraylist.add("b");
+        arraylist.add("c");
+       assertThat(arraylist.indexOf("b")).isEqualTo(1);
+    }
+    @Test
+    public int testLastIndexOf(){
+        List<String> arraylist=new CoArrayList();
+        arraylist.add("a");
+        arraylist.add("b");
+        arraylist.add("c");
+        assertThat(arraylist.lastIndexOf("b")).isEqualTo(1);
+    }
+    @Test
+    public void testIsEmpty(){
+        List<String> arraylist=new CoArrayList();
+        arraylist.add("a");
+        assertThat(arraylist.isEmpty()).isEqualTo(1);
+    }
+
+    @Test
+    public void testSubList(){
+        List<String> arraylist=new CoArrayList();
+        List<String> testlist=new ArrayList();
+        arraylist.add("a");
+        arraylist.add("b");
+        arraylist.add("c");
+        arraylist.add("d");
+        arraylist.add("e");
+        arraylist.add("f");
+        testlist.subList(1,3);
+        assertThat(testlist.size()).isEqualTo(2);
+    }
+
 }
